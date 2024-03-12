ad6e90c0ab3442297c145d9064422e75a9a40aa7
00013891 <114651187+00013891@users.noreply.github.com>
2024-03-12 21:16:41 +0500

    Finish ExerciseController creation

---
+++ WEBAPI_00013891/WEBAPI_00013891/Controllers/ExerciseController.cs
@@ -0,0 +1,88 @@
+﻿using DataAccessLayer_00013891;
+using DataAccessLayer_00013891.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI_00013891.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExerciseController : ControllerBase
+    {
+        public readonly IExerciseRepository _exerciseRepository;
+
+        public ExerciseController(IExerciseRepository exerciseRepository)
+        {
+            _exerciseRepository = exerciseRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_exerciseRepository.GetExercises());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(_exerciseRepository.GetExerciseById(id));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(int id, Exercise exercise)
+        {
+            try
+            {
+                _exerciseRepository.EditExercise(id, exercise);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(Exercise exercise)
+        {
+            try
+            {
+                _exerciseRepository.CreateExercise(exercise);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _exerciseRepository.DeleteExercise(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ WEBAPI_00013891/WEBAPI_00013891/Controllers/WorkoutController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI_00013891.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkoutController : ControllerBase
+    {
+    }
+}
