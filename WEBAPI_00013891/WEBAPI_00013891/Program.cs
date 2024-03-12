ff1c265a75c6a51653af556be6ece0a1e4893914
00013891 <114651187+00013891@users.noreply.github.com>
2024-03-12 21:13:20 +0500

    API settings

--- WEBAPI_00013891/WEBAPI_00013891/Program.cs
+++ WEBAPI_00013891/WEBAPI_00013891/Program.cs
@@ -1,3 +1,7 @@
+using DataAccessLayer_00013891;
+using DataAccessLayer_00013891.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<DataContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IExerciseRepository, ExerciseRepository>();
+builder.Services.AddTransient<IWorkoutRepository, WorkoutRepository>();
+
+builder.Services.AddCors(o =>
+    o.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        }));
 
 var app = builder.Build();
 
@@ -16,6 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAllOrigins");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
